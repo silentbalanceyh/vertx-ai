5a9a7ae05f263fbeb7a831ad07e789f0c711c160
Lang <silentbalanceyh@126.com>
2018-05-16 07:31:46 +0800

    资源文件

--- src/generator/model/field.js
+++ src/generator/model/field.js
@@ -1,8 +1,9 @@
 module.exports = class Field {
     constructor(expression = "") {
-        this.name = expression.split(',')[0].replace(/ /g, '');
-        this.display = expression.split(',')[1].replace(/ /g, '');
-        const mean = expression.split(',')[2];
+        const meta = expression.split(',');
+        this.name = meta[0].replace(/ /g, '');
+        this.display = meta[1].replace(/ /g, '');
+        const mean = meta[2];
         if (0 < mean.indexOf("S")) {
             this.isFilter = true;
         }
@@ -11,6 +12,19 @@ module.exports = class Field {
         }
         if (0 < mean.indexOf("C")) {
             this.isColumn = true;
+        }
+        // 读取后续参数对
+        if (meta[3]) {
+            const attrs = {};
+            for (let idx = 3; idx < meta.length; idx++) {
+                const kv = meta[idx].replace(/ /g, '');
+                const key = kv.split("=")[0];
+                const value = kv.split("=")[1];
+                if (key && value) {
+                    attrs[key] = value;
+                }
+            }
+            this.config = attrs;
         }
     }
 };
--- src/generator/page-list.js
+++ src/generator/page-list.js
@@ -39,6 +39,69 @@ const getFilter = (module = {}, cab = "") => {
     const grid = new Grid(3, content);
     return moduleTpl.replace(/#ROW#/g, grid.matrix);
 };
+
+const getList = (module = {}, cab = "") => {
+    const moduleTpl = fs.readFileSync(cab + "/UI.List.zt", "utf-8");
+    const columnTpl = cab + '/UI.List.Column.zt';
+    // 遍历字段
+    const content = [];
+    const condition = {};
+    eachField(module, (field) => {
+        if (field.isColumn) {
+            const colTpl = fs.readFileSync(columnTpl, "utf-8");
+            const colData = colTpl.replace(/#NAME#/g, field.name).replace(/#DISPLAY#/g, field.display);
+            let col = JSON.parse(colData);
+            // 是否排序
+            if (field.config['sorter']) {
+                col['sorter'] = field.config['sorter'];
+            }
+            // 文件合并
+            if (field.config['file']) {
+                const data = JSON.parse(fs.readFileSync(cab + '/tpl/' + field.config['file'] + '.json'));
+                Object.assign(col, data);
+            }
+            content.push(col);
+            // 是否过滤
+            if (field.config['filter']) {
+                condition[field.name] = field.config['filter'];
+            }
+        }
+    });
+    const rest = module.code.toLocaleLowerCase();
+    const moduleName = module.name;
+    let columns = JSON.stringify(content);
+    columns = columns.substring(1, columns.length - 1);
+    return moduleTpl
+        .replace(/#REST#/g, rest)
+        .replace(/#MODULE#/g, moduleName)
+        .replace(/#COLUMN#/g, columns)
+        .replace(/#COND#/g, JSON.stringify(condition));
+};
+
+const createFile = (targetFile, fnContent, prefix) => {
+    const content = fnContent();
+    if (content) {
+        fs.writeFileSync(targetFile, content);
+        const json = JSON.parse(fs.readFileSync(targetFile, "utf-8"));
+        fs.writeFileSync(targetFile, JSON.stringify(json, null, 4));
+        log.info(`${prefix}：${targetFile}`);
+    }
+};
+// UI.json
+const createUIJson = (tplFolder, cabPath, module = {}) => {
+    createFile(cabPath + "/UI.json", () => {
+        const index = fs.readFileSync(tplFolder + 'UI.zt', 'utf-8');
+        return index.replace(/#MODULE#/g, `${module.name}管理`);
+    }, "资源文件");
+};
+// UI.Filter.json
+const createUIFilterJson = (tplFolder, cabPath, module = {}) => {
+    createFile(cabPath + '/UI.Filter.json', () => getFilter(module, tplFolder), "资源文件");
+};
+// UI.List.json
+const createUIListJson = (tplFolder, cabPath, module = {}) => {
+    createFile(cabPath + '/UI.List.json', () => getList(module, tplFolder), "资源文件");
+};
 exports.createPlist = function () {
     const argv = args.parseArgs(2);
     const inputFile = argv['-c'] || argv['--config'];
@@ -59,12 +122,13 @@ exports.createPlist = function () {
             const comPath = `src/components${path}`;
             io.dirsMake(comPath);
             log.info(`组件目录创建成功：${comPath}`);
+            const folder = __dirname + `/tpl/page-list/cab/${language}/`;
+            // 构造UI.json
+            createUIJson(folder, cabPath, module);
             // 构造UI.Filter.json
-            const filterFolder = __dirname + `/tpl/page-list/cab/${language}/`;
-            const filterFile = cabPath + '/UI.Filter.json';
-            const filter = getFilter(module, filterFolder);
-            fs.writeFileSync(filterFile, filter);
-            log.info(`资源文件：${filterFile}`);
+            createUIFilterJson(folder, cabPath, module);
+            // 构造UI.List.json
+            createUIListJson(folder, cabPath, module);
         }
         else {
             log.error(`数据格式不合法，请检查配置文件${configPath}`);
