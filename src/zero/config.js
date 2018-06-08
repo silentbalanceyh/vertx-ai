39a7f29cf46833392938ff438690370eae0573c0
Lang <silentbalanceyh@126.com>
2018-06-08 13:29:04 +0800

    提供数据生成功能

--- src/zero/config.js
+++ src/zero/config.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const Mock = require("mockjs");
 const {v4} = require("uuid");
-const randomjs = require('random-js');
 const Field = require("../generator/model/field");
 const log = require('./log');
+const Random = Mock.Random;
 const execFileExist = (configFile, callback) => {
     if (fs.existsSync(configFile)) {
         log.info(`从配置文件初始化，配置文件路径: ${configFile}.`);
@@ -28,21 +29,36 @@ const eachField = (module = {}, callback) => {
         process.exit();
     }
 };
+const dataGenerator = {
+    "Code": () => Random.string('ABCDEFGHIJKLMNOPQRSTUVWXYZ.', 6),
+    "CnCompany": () => Random.ctitle() + "企业",
+    "EnCompany": () => Random.title(1, 3) + " Company",
+    "CnUser": () => Random.cname(),
+    "HeadCount": () => Random.natural(10, 1000),
+    "Web": () => Random.url('http'),
+    "Scope": () => "范围" + Random.ctitle(4),
+    "Email": () => Random.email(),
+    "CnAddress": () => Random.region() + Random.county(true) + Random.ctitle(),
+    "EnAddress": () => Random.first() + ', State ' + Random.title(1) + ", Street " + Random.last(),
+    "Mobile": () => "1" + Random.string("123456789", 10),
+    "Phone": () => "(0" + Random.string("0123456789", 2) + ") " + Random.string("0123456789", 4) + " " + Random.string("0123456789", 4)
+};
 const execData = (module) => {
     const record = {};
     record.key = v4();
     if (module.data) {
-        const engine = randomjs.engines.mt19937().autoSeed();
-        const flag = randomjs.string()(engine, 2);
         for (const key in module.data) {
             if (module.data.hasOwnProperty(key)) {
-                record[key] = module.data[key] + flag;
+                const generator = module.data[key];
+                if (dataGenerator[generator]) {
+                    record[key] = dataGenerator[generator]()
+                }
             }
         }
     }
     // 特殊内容处理
-    record.createDate = new Date().toISOString().split('.')[0];
-    record.lastModifiedDate = new Date().toISOString().split('.')[0];
+    if (!record.createDate) record.createDate = new Date().toISOString().split('.')[0];
+    if (!record.lastModifiedDate) record.lastModifiedDate = new Date().toISOString().split('.')[0];
     record.active = true;
     return record;
 };
