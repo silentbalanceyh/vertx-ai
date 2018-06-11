e598c0b87d675fd046d0d18ddc8fcec4fdfe36a1
Lang <silentbalanceyh@126.com>
2018-06-11 14:28:55 +0800

    添加key设置

--- src/generator/key-apply.js
+++ src/generator/key-apply.js
@@ -3,18 +3,26 @@ const {args, log, config} = ai;
 const fs = require("fs");
 const uuid = require("uuid");
 
+const applyKey = (data = {}) => {
+    for (const key in data) {
+        const value = data[key];
+        if ("object" === typeof value) {
+            if (Array.prototype.isPrototypeOf(value)) {
+                value.forEach(item => applyKey(item));
+            } else {
+                applyKey(value);
+            }
+        }
+    }
+    if (!data.key) data.key = uuid();
+};
+
 exports.applyKey = function () {
     const argv = args.parseArgs(2);
     const inputFile = argv['-d'] || argv['--data'];
     config.execFileExist(inputFile, (configPath) => {
         const configData = JSON.parse(fs.readFileSync(configPath));
-        if (configData.data) {
-            if (Array.prototype.isPrototypeOf(configData.data)) {
-                configData.data.forEach(item => !item.hasOwnProperty('key') ? item.key = uuid() : item.key);
-            } else {
-                configData.data.key = !configData.data.key ? uuid() : configData.data.key;
-            }
-        }
+        applyKey(configData.data);
         log.info(`写入数据到${inputFile}`);
         fs.writeFileSync(inputFile, JSON.stringify(configData, null, 4), "utf-8");
     })
