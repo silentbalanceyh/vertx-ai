ecd0e4f4286997c9c78c26ec2728eec9db8e6fa1
Lang <silentbalanceyh@126.com>
2018-06-23 01:04:01 +0800

    添加新命令

--- src/executor.js
+++ src/executor.js
@@ -2,11 +2,13 @@ const menu = require('./generator/menu.js');
 const pageList = require('./generator/page-list');
 const searchList = require('./generator/search-list');
 const tabList = require("./generator/tab-list");
+const fullList = require('./generator/full-list');
 const keyApply = require("./generator/key-apply");
 module.exports = {
     ...menu,
     ...pageList,
     ...searchList,
     ...tabList,
+    ...fullList,
     ...keyApply
 };
--- src/generator/key-apply.js
+++ src/generator/key-apply.js
@@ -3,27 +3,50 @@ const {args, log, config} = ai;
 const fs = require("fs");
 const uuid = require("uuid");
 
-const applyKey = (data = {}) => {
+const applyKey = (data = {}, field = 'key') => {
     for (const key in data) {
         const value = data[key];
         if ("object" === typeof value) {
             if (Array.prototype.isPrototypeOf(value)) {
-                value.forEach(item => applyKey(item));
+                value.forEach(item => applyKey(item, field));
             } else {
-                applyKey(value);
+                applyKey(value, field);
             }
         }
     }
-    if (!data.key) data.key = uuid();
+    if (!data.hasOwnProperty('$REF$')) {
+        if (!data[field]) data[field] = uuid();
+    }
+};
+
+const eachFile = (path, callback) => {
+    const etat = fs.statSync(path);
+    if (etat.isDirectory()) {
+        const dir = fs.readdirSync(path);
+        for (let idx = 0; idx < dir.length; idx++) {
+            if (!dir[idx].startsWith("_")) {
+                let hitFile = null;
+                if (path.endsWith('/')) {
+                    hitFile = path + dir[idx];
+                } else {
+                    hitFile = path + '/' + dir[idx];
+                }
+                eachFile(hitFile, callback);
+            }
+        }
+    } else {
+        callback(path);
+    }
 };
 
 exports.applyKey = function () {
-    const argv = args.parseArgs(2);
+    const argv = args.parseArgs(4);
     const inputFile = argv['-d'] || argv['--data'];
-    config.execFileExist(inputFile, (configPath) => {
-        const configData = JSON.parse(fs.readFileSync(configPath));
-        applyKey(configData.data);
-        log.info(`写入数据到${inputFile}`);
-        fs.writeFileSync(inputFile, JSON.stringify(configData, null, 4), "utf-8");
-    })
+    const inputField = argv['-f'] || argv['--field'];
+    config.execFileExist(inputFile, (configPath) => eachFile(configPath, (file) => {
+        const configData = JSON.parse(fs.readFileSync(file));
+        applyKey(configData.data, inputField);
+        log.info(`写入数据到${file}`);
+        fs.writeFileSync(file, JSON.stringify(configData, null, 4), "utf-8");
+    }))
 };
