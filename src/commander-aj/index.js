092bb2416272e60d1b5882c1647430c3df4ee9d0
Lang <silentbalanceyh@126.com>
2021-10-23 13:04:37 +0800

    ai

--- src/commander-aj/index.js
+++ src/commander-aj/index.js
@@ -2,10 +2,12 @@ const reactComplex = require('./fn.i.complex');
 const reactForm = require('./fn.i.form');
 const javaPermission = require('./fn.j.permission');
 const javaPlugin = require('./fn.j.plugin');
+const javaRow = require('./fn.j.row');
 const exported = {
     reactComplex,
     reactForm,
     javaPermission,
-    javaPlugin
+    javaPlugin,
+    javaRow
 };
 module.exports = exported;
