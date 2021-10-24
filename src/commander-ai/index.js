dcf73cb02cfa6e81db52ea22af62930ba790b1ab
Lang <silentbalanceyh@126.com>
2021-10-24 09:34:12 +0800

    0.3.3 Release

--- src/commander-ai/index.js
+++ src/commander-ai/index.js
@@ -3,11 +3,13 @@ const executeCsv = require('./fn.csv');
 const executeUk = require('./fn.uk');
 const executeKey = require('./fn.key');
 const executeData = require('./fn.data');
+const executeInit = require('./fn.init');
 const exported = {
     executeUuid,        // ai uuid
     executeCsv,         // ai csv
     executeUk,          // ai uk
     executeKey,         // ai key
     executeData,        // ai data
+    executeInit,        // ai init
 };
 module.exports = exported;
