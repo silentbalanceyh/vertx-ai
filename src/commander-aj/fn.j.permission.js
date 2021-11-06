33ab143918a4a6ec679d33bfbff5f3128b8b6436
Lang <silentbalanceyh@126.com>
2021-11-06 20:56:09 +0800

    jperm

--- src/commander-aj/fn.j.permission.js
+++ src/commander-aj/fn.j.permission.js
@@ -1,19 +1,33 @@
 const Ec = require('../epic');
+const path = require('path');
 
 module.exports = () => {
     const actual = Ec.executeInput(
+        [
+            ["-r", "--role"]
+        ],
         [
             ["-r", "--role"],
             ["-i", "--input"]
         ]
     );
+    // 读取文件列表
+    const standard = Ec.ioFiles(path.join(__dirname, `../cab/perm`));
+    const configuration = {};
+    configuration.files = standard.map(item => item.path);
+    if (actual.input) {
+        let filesInput = Ec.ioFiles(path.join(process.cwd(), actual.input));
+        configuration.filesInput = filesInput.map(item => item.path);
+    }
+    configuration.role = actual.role;
+    Ec.excelRun(configuration);
 }
 /**
  * ## `aj jperm`
  *
  * ### 1. 命令
  *
- * 使用该命令生成权限模板专用表格数据，初始化权限信息专用。
+ * 使用该命令生成某个角色的专用权限文件目录（内置所有Zero Extension标准权限）
  *
  * ### 2. 执行
  *
@@ -21,47 +35,13 @@ module.exports = () => {
  *
  * ```shell
  * # 2.1.1. 命令语法
- * aj jperm -c <config>
+ * aj jperm -r <role> -i <input>
  *
  * # 2.1.2. 执行测试
- * ai jperm
- * # ...省略部分...
- * [Zero AI] Zero AI 系统启动......
- * [Zero AI] Zero AI 加载输入参数：
- * {
- *      "config": "workspace.json"
- * }
- * [Zero AI] 「Java环境」，后端工作路径：<工作目录>。
- * [Zero AI] Zero AI  0. 基础环境......
- * [Zero AI] Zero AI  工作目录：<工作目录>
- * [Zero AI] Zero AI  1. 执行Excel命令......
- * [Zero AI] Zero AI  1.1. 加载数据中......
- * [Zero AI] Zero AI  读取原始文件：<Vert-AI>/src/cab/perm/permission.xlsx
- * [Zero AI] Zero AI  执行Worksheet：DATA-PERM。
- * [Zero AI] Zero AI  分析结果：最大行 - 67，最大列 - 12。
- * [Zero AI] Zero AI  参数表：
- * [Zero AI] Zero AI       MODULE = 业务模块名称
- * [Zero AI] Zero AI       API = URI路径替换标识符
- * [Zero AI] Zero AI       SIGMA = SIGMA统一标识符
- * [Zero AI] Zero AI       IDENTIFIER = 模型统一标识符，对应identifier
- * [Zero AI] Zero AI       PREFIX = 生成文件名前缀
- * [Zero AI] Zero AI       ROLE_ID = 系统中的角色ID
- * [Zero AI] Zero AI       SHEET = 操作的Sheet名称
- * [Zero AI] Zero AI  1.2. 更新数据......
- * [Zero AI] Zero AI  GUID单元格 -         36
- * [Zero AI] Zero AI  数据单元格 -         141
- * [Zero AI] Zero AI  待检查单元格 -       0，（ERROR）
- * [Zero AI] Zero AI  跳过单元格 -         141
- * [Zero AI] Zero AI  1.3. 创建新数据文件......
- * [Zero AI] Zero AI  执行Worksheet：<工作目录>/src/main/resources/init/oob/<生成文件名>.xlsx。
- * [Zero AI] Zero AI  2. 命令执行完成！！！
- * ```
+ * ai jperm -r ccc -i input
  *
- * 代码执行完成后会在后端工作目录中生成权限文件（可直接导入）。
- *
- * * 执行代码之前可使用`export ZF=XXX`设置后端项目主目录，否则使用配置文件中的`ZF`。
- * * 不支持SIGMA的`formula`替换流程，原始`environment.ambient.xlsx`的引用此处直接消除。
- * * 只生成权限专用的内容，以模块为中心，生成单独模块的权限配置。
+ * [Zero AI] Zero AI  1. 准备生成角色对应权限：ID = "ccc" ...
+ * [Zero AI] Zero AI  2. 生成 Zero Extension 权限...
  *
  * ### 3. 选项
  *
@@ -69,7 +49,8 @@ module.exports = () => {
  *
  * |短参|全参|类型|默认|含义|
  * |---|---|---|---|:---|
- * |-c|--config|String|`workspace.json`|配置文件路径。|
+ * |-c|--role|String|无|系统中角色的UUID。|
+ * |-i|--input|String|无|另外一个目录，用来合并新权限。|
  *
  * @memberOf module:aj
  * @method jperm
--- src/epic/ai.backend.excel.js
+++ src/epic/ai.backend.excel.js
@@ -1,6 +1,6 @@
 const Log = require('./ai.export.log');
 const Word = require('./ai.export.word');
-
+const Io = require('./ai.export.io');
 const Excel = require('exceljs');
 const path = require('path');
 // ==========================================================
@@ -76,14 +76,46 @@ const excelTpl = async (config = {}) => {
     }
     return workbook;
 }
+
+const excelLog = (index, message) => Log.info(`Zero AI `.cyan + `「${index}」`.red + message)
+const excelGenerate = async (role, file, index, prefix) => {
+    excelLog(index, ` -> 加载数据中`);
+    const workbook = new Excel.Workbook();
+    await workbook.xlsx.readFile(file);
+    excelLog(index, ` 读取原始文件：${file.blue}`)
+    if (workbook) {
+        const worksheetRef = workbook.getWorksheet("DATA-PERM");
+        if (worksheetRef) {
+            const maxRow = worksheetRef['_rows'].length;
+            const maxColumn = worksheetRef['_columns'].length;
+            excelLog(index, ` 分析结果：最大行 - ${maxRow.toString().blue}，最大列 - ${maxColumn.toString().blue}。`);
+            worksheetRef.eachRow((rowRef, rowNumber) => {
+                rowRef.eachCell((cellRef, cellNumber) => {
+                    const original = cellRef.value;
+                    if ("#ROLE_ID#" === original) {
+                        cellRef.value = role;
+                    }
+                })
+            })
+            const segment = file.split('/');
+            const filename = segment[segment.length - 1];
+            const target = prefix ? `ROLE-${role}/${prefix}.${filename}` : `ROLE-${role}/${filename}`;
+            excelLog(index, `创建新数据文件 ${target}`);
+            await workbook.xlsx.writeFile(target);
+        } else {
+            excelLog(index, `文件读取有问题：${file}`.red);
+        }
+    }
+}
 const excelRun = (config = {}) => {
-    Log.info(`Zero AI `.cyan + ` 1. 执行Excel命令......`.rainbow);
+    Log.info(`Zero AI `.cyan + ` 1. 准备生成角色对应权限：ID = "${config.role.green}" ...`);
+    Io.dirCreate(`${process.cwd()}/ROLE-${config.role}`);
     // 1. 执行替换
-    excelTpl(config).then(() => {
-        Log.info(`Zero AI `.cyan + ` 2. 命令执行完成！！！`.rainbow);
-    }).catch(error => {
-        console.error(error);
-    });
+    const {files = [], filesInput = []} = config;
+    Log.info(`Zero AI `.cyan + ` 2. 生成 Zero Extension 权限...`.yellow);
+    files.forEach((file, index) => excelGenerate(config.role, file, index))
+    Log.info(`Zero AI `.cyan + ` 3. 生成 输入文件 权限...`.yellow);
+    filesInput.forEach((file, index) => excelGenerate(config.role, file, index, "input"))
 }
 module.exports = {
     excelRun,
